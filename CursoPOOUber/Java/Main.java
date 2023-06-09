bf03f54f94be26393170d4cb0aa50af204e52533
pjourdan <pauljourdanbaez@gmail.com>
2023-06-09 15:57:10 -0400

    Cambios para declarar clases y utilizarlos en java y python

--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,5 +1,15 @@
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Inicia el codigo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        car.printDataCar();
+        Car car2 = new Car();
+        car2.license = "QGH726";
+        car2.driver = "Andra Herrera";
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,2 +1,12 @@
+from car import Car
+
 if __name__ == "__main__":
-    print("Hola Mundo")
+    print("Inicia el codigo")
+    car = Car()
+    car.license = "AMG456"
+    car.driver = "Andres Garza"
+    print(vars(car))
+    car2 = Car()
+    car2.license = "REW923"
+    car2.driver = "Ricardo Martinez"
+    print(vars(car2))
