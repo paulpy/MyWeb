c97383ab0d1df7fc7c15085b27268102e52e91fc
pjourdan <pauljourdanbaez@gmail.com>
2023-06-09 16:49:22 -0400

    Cambios para trabajar en python

--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,12 +1,8 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Inicia el codigo")
-    car = Car()
-    car.license = "AMG456"
-    car.driver = "Andres Garza"
+    car = Car("AMG456", Account("Carlos Gallano", "3234EERT"))
     print(vars(car))
-    car2 = Car()
-    car2.license = "REW923"
-    car2.driver = "Ricardo Martinez"
-    print(vars(car2))
+    print(vars(car.driver))
