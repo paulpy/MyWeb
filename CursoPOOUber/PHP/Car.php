9c3fd648749c43910f745967dd55a95936bb7e5a
pjourdan <pauljourdanbaez@gmail.com>
2023-06-09 16:57:57 -0400

    Cambios para php

--- CursoPOOUber/PHP/Car.php
+++ CursoPOOUber/PHP/Car.php
@@ -1,8 +1,18 @@
 <?php
+require_once 'Account.php'
 class Car{
     public $id;
     public $license;
     public $driver;
     public $passenger;
+
+    public function __construct($license, Account $driver){
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    publicfunctionPrintDataCar(){
+        echo"license: $this->license, conductor: {$this->driver->name}, document: {$this->driver->document}";
+      }
 }
 ?>
