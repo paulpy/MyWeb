3e5ac006e50b6231e2a9f4ba4c35e4e2d0e7f78f
pjourdan <pauljourdanbaez@gmail.com>
2023-06-10 21:50:58 -0400

    Arreglo de Clase UberX

--- CursoPOOUber/Java/UberX.java
+++ CursoPOOUber/Java/UberX.java
@@ -2,7 +2,9 @@ class UberX extends Car {
     String brand;
     String model;
 
-    public UberX(String license, Account driver){
+    public UberX(String license, Account driver, String brand, String model){
         super(license, driver);
+        this.brand = brand;
+        this.model = model;
     }
 }
